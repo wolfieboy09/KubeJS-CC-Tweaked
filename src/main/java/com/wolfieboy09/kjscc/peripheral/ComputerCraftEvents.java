e96ad9e0c92672cc268cd40b7280fcae902ab1c6
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-04-09 18:54:52 -0400

    In pain of programming

---
+++ src/main/java/com/wolfieboy09/kjscc/peripheral/ComputerCraftEvents.java
@@ -0,0 +1,11 @@
+package com.wolfieboy09.kjscc.peripheral;
+
+
+import dev.latvian.mods.kubejs.bindings.event.ServerEvents;
+import dev.latvian.mods.kubejs.event.EventGroup;
+import dev.latvian.mods.kubejs.event.EventHandler;
+
+
+public class ComputerCraftEvents {
+    public static final EventGroup GROUP = EventGroup.of("ComputerCraftEvents");
+}
