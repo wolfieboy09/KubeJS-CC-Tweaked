2b0eaa3ca59c94a105e483cec7f1f1c966fb9da5
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-04-09 09:47:59 -0400

    Initial Commit

--- common/src/main/java/com/prunoideae/kjscc/KJSCC.java
+++
@@ -1,9 +0,0 @@
-package com.prunoideae.kjscc;
-
-public class KJSCC {
-    public static final String MOD_ID = "kjscc";
-
-    public static void init() {
-
-    }
-}
--- common/src/main/java/com/prunoideae/kjscc/peripheral/PeripheralMethod.java
+++
@@ -1,7 +0,0 @@
-package com.prunoideae.kjscc.peripheral;
-
-public record PeripheralMethod(String type, DynamicPeripheralJS.PeripheralCallback callback, boolean mainThread) {
-    public PeripheralMethod(String type, DynamicPeripheralJS.PeripheralCallback callback) {
-        this(type, callback, false);
-    }
-}
