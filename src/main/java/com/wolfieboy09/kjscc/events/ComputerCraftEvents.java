0c963205d5915e1dc15a6699a5b09022f3a1b5b7
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-04-10 16:57:34 -0400

    Removed unused import statement

--- src/main/java/com/wolfieboy09/kjscc/events/ComputerCraftEvents.java
+++ src/main/java/com/wolfieboy09/kjscc/events/ComputerCraftEvents.java
@@ -4,6 +4,7 @@ package com.wolfieboy09.kjscc.events;
 import com.wolfieboy09.kjscc.peripheral.PeripheralJS;
 import dev.latvian.mods.kubejs.event.EventGroup;
 import dev.latvian.mods.kubejs.event.EventHandler;
+import dev.latvian.mods.kubejs.event.StartupEventJS;
 
 public class ComputerCraftEvents {
     public static final EventGroup GROUP = EventGroup.of("ComputerCraftEvents");
