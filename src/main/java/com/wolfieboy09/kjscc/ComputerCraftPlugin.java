413a14cbd12762ff1d35876e1359c39c9e8a95cf
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-04-10 16:42:11 -0400

    Suppliers are my enemy

--- src/main/java/com/wolfieboy09/kjscc/ComputerCraftPlugin.java
+++ src/main/java/com/wolfieboy09/kjscc/ComputerCraftPlugin.java
@@ -1,5 +1,6 @@
 package com.wolfieboy09.kjscc;
 
+import com.wolfieboy09.kjscc.events.ComputerCraftEvents;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.latvian.mods.kubejs.script.BindingsEvent;
 import org.jetbrains.annotations.NotNull;
@@ -8,4 +9,10 @@ public class ComputerCraftPlugin extends KubeJSPlugin {
     public void addBindings(@NotNull BindingsEvent event) {
         event.add("Lua", LuaJS.class);
     }
+
+    @Override
+    public void registerEvents() {
+        ComputerCraftEvents.GROUP.register();
+    }
+
 }
--- src/main/java/com/wolfieboy09/kjscc/events/ComputerCraftEvents.java
+++ src/main/java/com/wolfieboy09/kjscc/events/ComputerCraftEvents.java
@@ -8,5 +8,5 @@ import dev.latvian.mods.kubejs.event.EventHandler;
 public class ComputerCraftEvents {
     public static final EventGroup GROUP = EventGroup.of("ComputerCraftEvents");
 
-    EventHandler PERIPHERAL = ComputerCraftEvents.GROUP.startup("peripheral", PeripheralJS.class);
+    EventHandler PERIPHERAL = ComputerCraftEvents.GROUP.startup("peripheral", () ->  PeripheralJS.class);
 }
