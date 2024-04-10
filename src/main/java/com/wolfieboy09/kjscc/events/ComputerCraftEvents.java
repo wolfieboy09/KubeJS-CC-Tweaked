8269971ea966eb5c51ebf032c7960f8b8330a03f
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-04-10 17:08:01 -0400

    Changed into an interface

--- src/main/java/com/wolfieboy09/kjscc/events/ComputerCraftEvents.java
+++ src/main/java/com/wolfieboy09/kjscc/events/ComputerCraftEvents.java
@@ -4,10 +4,9 @@ package com.wolfieboy09.kjscc.events;
 import com.wolfieboy09.kjscc.peripheral.PeripheralJS;
 import dev.latvian.mods.kubejs.event.EventGroup;
 import dev.latvian.mods.kubejs.event.EventHandler;
-import dev.latvian.mods.kubejs.event.StartupEventJS;
 
-public class ComputerCraftEvents {
-    public static final EventGroup GROUP = EventGroup.of("ComputerCraftEvents");
+public interface ComputerCraftEvents {
+    EventGroup GROUP = EventGroup.of("ComputerCraftEvents");
 
     EventHandler PERIPHERAL = ComputerCraftEvents.GROUP.startup("peripheral", () ->  PeripheralJS.class);
 }
