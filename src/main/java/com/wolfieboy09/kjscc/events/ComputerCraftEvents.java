6c1a47f9770553929c90a1648a8da440dd138244
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-04-11 18:08:53 -0400

    less line

--- src/main/java/com/wolfieboy09/kjscc/events/ComputerCraftEvents.java
+++ src/main/java/com/wolfieboy09/kjscc/events/ComputerCraftEvents.java
@@ -1,6 +1,5 @@
 package com.wolfieboy09.kjscc.events;
 
-
 import com.wolfieboy09.kjscc.peripheral.PeripheralJS;
 import dev.latvian.mods.kubejs.event.EventGroup;
 import dev.latvian.mods.kubejs.event.EventHandler;
