64ff5272092051f186be3cb603f0751fa09145fd
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-05-01 14:59:17 -0400

    converted to record class

--- src/main/java/com/wolfieboy09/kjscc/result/MarkerUnpackedResult.java
+++ src/main/java/com/wolfieboy09/kjscc/result/MarkerUnpackedResult.java
@@ -1,13 +1,3 @@
 package com.wolfieboy09.kjscc.result;
 
-public class MarkerUnpackedResult {
-    private final Object[] results;
-
-    public MarkerUnpackedResult(Object... results) {
-        this.results = results;
-    }
-
-    public Object[] getResults() {
-        return results;
-    }
-}
+public record MarkerUnpackedResult(Object... results) { }
