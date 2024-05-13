7330ddd634e9a8484ee2838e93dfa7c98460d71b
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-05-13 12:16:46 -0400

    Lot more has been done. Need to get "You are using an outdated version of Gradle (8.1.1). Gradle 8.6 or higher is required." error fixed. More files soon

---
+++ fabric/src/main/java/com/wolfieboy09/KJSCC/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package com.wolfieboy09.KJSCC.platform;
+
+import com.wolfieboy09.KJSCC.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
--- src/main/java/com/wolfieboy09/kjscc/peripheral/PeripheralMethod.java
+++
@@ -1,7 +0,0 @@
-package com.wolfieboy09.kjscc.peripheral;
-
-public record PeripheralMethod(String type, DynamicPeripheralJS.PeripheralCallback callback, boolean mainThread) {
-    public PeripheralMethod(String type, DynamicPeripheralJS.PeripheralCallback callback) {
-        this(type, callback, false);
-    }
-}
