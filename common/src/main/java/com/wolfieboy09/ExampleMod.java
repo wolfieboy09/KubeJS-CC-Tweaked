be1ae260d7fdb1b866f7976390d314096189a953
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-05-23 16:19:12 -0400

    time to actually start it again

---
+++ common/src/main/java/com/wolfieboy09/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.wolfieboy09;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "kjscc";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
